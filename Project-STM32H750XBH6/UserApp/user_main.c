0645e698480cdf8c6cc43242f9f71576584d1865
Neon <oo0-.-0oo@outlook.com>
2023-12-25 21:30:51 +0800

    update

--- Project-STM32H750XBH6/UserApp/user_main.c
+++ Project-STM32H750XBH6/UserApp/user_main.c
@@ -1,9 +1,12 @@
 #include "user_main.h"
+#include "main.h"
 
-
-void user_main(void)
+void user_main()
 {
-
+    while (1){
+        HAL_Delay(100);
+        HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_8);
+    }
 }
 
 
